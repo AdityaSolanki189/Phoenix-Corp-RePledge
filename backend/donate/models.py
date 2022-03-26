386e9a5b6facf7c9f6e658f1d47d0ad4d099dacf
Aditya Solanki <aditya2ss283@gmail.com>
2022-03-27 00:30:08 +0530

    added trackers backend

--- backend/donate/models.py
+++ backend/donate/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator
 from datetime import *
+from django.db.models.signals import post_save
 
 # Create your models here.
 class donate(models.Model):
@@ -13,3 +14,26 @@ class donate(models.Model):
     ngo = models.CharField(max_length=300,blank=True)
     location = models.CharField(max_length=500) 
     location_link = models.URLField(max_length=500,blank=True)
+    def __str__(self) -> str:
+        return self.name
+
+class tracker(models.Model):
+    choices = (
+        ("OP", "Order Placed"),
+        ("IT", "In Transit"),
+        ("OT", "Output Delivery"),
+        ("DE", "Delivery"),
+    )
+    donate = models.ForeignKey(donate,on_delete=models.CASCADE,related_name="tracker")
+    desc = models.CharField(max_length=256,blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2,choices=choices,default="OP")
+    def __str__(self) -> str:
+        return self.donate.name + " " + self.status
+def post_save_tracker_model_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        try:
+            tracker.objects.create(donate=instance,desc="donation request received")
+        except:
+            pass
+post_save.connect(post_save_tracker_model_receiver, sender=donate)
