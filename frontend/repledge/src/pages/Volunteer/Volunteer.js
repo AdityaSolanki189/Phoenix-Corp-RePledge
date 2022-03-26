f10d2f19cd368b306c8075c5848952dc0688520c
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2022-03-27 02:35:03 +0530

    volunteer page done

--- frontend/repledge/src/pages/Volunteer/Volunteer.js
+++ frontend/repledge/src/pages/Volunteer/Volunteer.js
@@ -4,27 +4,47 @@ import {sliderData} from '../Home/Home'
 import './Volunteer.css'
 
 function Volunteer() {
-  return (
+    return (
 
-    <div className='volunteer_parent'>
-        <div className='grid'> 
-            {sliderData.map(item => {
-            return (
-                <div className="nayaCard">
-                    <div className='imageView'>
-                        <img src={item.image}/>
-                    </div>
-                    <div className='heading'>
-                        <p>{item.topic}</p>
+        <div className='volunteer_parent'>
+
+        <h1>Drives around you</h1>
+
+            <div className='grid'>
+                {sliderData.map(item => {
+                    return (
+                        <div className="nayaCard">
+                            <div className='imageView'>
+                                <img src={item.image}/>
+                            </div>
+                            <div className='heading'>
+                                <h3>{item.topic}</h3>
+                            </div>
+
+                            <div className="info">
+
+                                {item.userImage
+                                    ? <img src={item.userImage}/>
+                                    : <span class="material-icons">account_circle</span>}
+                                <span>{item.userName}</span>
+                            </div>
+
+                            <div className="location">
+                                <span class="material-icons">
+                                    location_on
+                                </span>
+                                <p>{item.location}</p>
+                            </div>
+
+                            <button className='volunteerButton'>Volunteer</button>
+
                         </div>
-                    
-                </div>
-            )
+                    )
 
-          })}
+                })}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Volunteer
