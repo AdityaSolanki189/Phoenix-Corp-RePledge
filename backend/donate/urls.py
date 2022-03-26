74566ac8f81c38759cb20e3829188d72f9a95619
tenserebel <premjha3010@gmail.com>
2022-03-26 22:56:08 +0530

    added donate and drives backend

--- backend/donate/urls.py
+++ backend/donate/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('donate/', views.index, name='index'),
     path('donate/<int:pk>/',views.details, name="details")
 ]
