ebe683d0fe976ac8abe00cec421da44eff176fea
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2022-03-26 13:34:56 +0530

    file rearrangement

---
+++ frontend/repledge/src/components/HeaderSignedIn/HeaderSignedIn.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import PersonIcon from './user_icon.png';
+import './HeaderSignedIn.css'
+
+function HeaderSignedIn() {
+    return (
+        <div className='main'>
+
+            <div className="logo">
+                Repledge
+            </div>
+
+            <div className="userIcon">
+                {/* <img src={PersonIcon} alt="img" /> */}
+                <span class="material-icons">
+                    account_circle
+                </span>
+            </div>
+
+        </div>
+    )
+}
+
+export default HeaderSignedIn;
